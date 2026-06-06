f3fb29b609bfb200c50f848c8f200d23b9799a91
kio662 <kiotechsalembranch@gmail.com>
2026-06-06 09:36:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,35 @@
-from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters
+import asyncio
+
+try:
+asyncio.get_event_loop()
+except RuntimeError:
+asyncio.set_event_loop(
+asyncio.new_event_loop()
+)
+
+from telegram.ext import (
+Application
+)
 
 from config import BOT_TOKEN
 
-
 def main():
-    app = Application.builder().token(BOT_TOKEN).build()
-    print('CinemaCityHub Started')
-    app.run_polling(drop_pending_updates=True)
 
+```
+app = (
+    Application.builder()
+    .token(BOT_TOKEN)
+    .build()
+)
 
-if __name__ == '__main__':
-    main()
+print(
+    "🎬 CinemaCityHub Started"
+)
+
+app.run_polling(
+    drop_pending_updates=True
+)
+```
+
+if **name** == "**main**":
+main()
